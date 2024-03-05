3175d0f6e5c0acbeb7a723368569eb4c79357406
nadavbrilon <63443963+nadavbrilon@users.noreply.github.com>
2024-03-05 16:15:07 +0200

    commit of project files

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import * as dotenv from "dotenv";
 // import with .js, and not ts.
 // for more info: https://devblogs.microsoft.com/typescript/announcing-typescript-4-7/#type-in-package-json-and-new-extensions
 import { createRoute, createComment, getComment, mainRoute } from "./routes.js";
-import { GET_COMMENT, POST_COMMENT } from "./const.js";
+import {GET_COMMENT, POST_COMMENT} from "./const.js";
+import * as zlib from "zlib";
 
 // For environment-variables
 dotenv.config();
@@ -13,14 +14,13 @@ const port = process.env.PORT || 3000;
 
 // Connect to mongoDB
 
-/* TODO: Replace with your connection string and add your DBPASS to a .env file */
-const dbURI = `mongodb+srv://wsp:${process.env.DBPASS}@cluster0.fn26nur.mongodb.net/?retryWrites=true&w=majority`;
-/* ========== */
+const dbURI = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.tzeo2jo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`/* ========== */
 
 await mongoose.connect(dbURI);
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   const route = createRoute(req.url, req.method);
+
 
   switch (route) {
     case POST_COMMENT:
@@ -30,10 +30,12 @@ const server = createServer((req: IncomingMessage, res: ServerResponse) => {
       getComment(req, res);
       break;
     default:
-      mainRoute(req, res);
+      // mainRoute(req, res);.
+      res.statusCode = 404; // Set status code to 404
+      res.end("Not Found"); // Send "Not Found" response
       break;
   }
 });
 
 server.listen(port);
-console.log(`Server running! port ${port}`);
+console.log(`Server running! port: ${port}`);
--- src/models/comment.ts
+++ src/models/comment.ts
@@ -6,12 +6,20 @@ import * as mongoose from "mongoose";
 // Creating new Schema object
 // Each propery have type filed - https://mongoosejs.com/docs/schematypes.html
 // And requierd files if needed
-const commentSchema = new mongoose.Schema(
-  /* TODO: Fill in your comment schema */
-
-  /* ========== */
+const commentSchema = new mongoose.Schema
+(
+    {
+        eventId: { type: String, required: true, min: 1 },
+        author: { type: String, required: true, min: 1},
+        content: { type: String, required: true, min: 1 },
+        createdAt: {
+            type: String,
+            required: true,
+            match: /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/, // regex pattern for YYYY-MM-DDTHH:MM format
+            message: 'Invalid date format'
+        }
+    }
 );
-
 // Models are fancy constructors compiled from Schema definitions. An instance of a model is called a document.
 // Models are responsible for creating and reading documents from the underlying MongoDB database.
 // https://mongoosejs.com/docs/models.html
